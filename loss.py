Fix the sign of softmax activation

loss.py
@@ -24,7 +24,7 @@ class CrossEntropyLoss(object):
     def backward(self):
         for i in range(len(self.grads) // 3, 0, -1):
             if i == len(self.grads) // 3:
-                self.grads["dZ" + str(i)] = -self.caches["A" + str(i)] + self.labels
+                self.grads["dZ" + str(i)] = self.caches["A" + str(i)] - self.labels
             else:
                 self.grads["dZ" + str(i)] = np.dot(self.parameters["W" + str(i + 1)].T,
                                                    self.grads["dZ" + str(i + 1)]) * \

model.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def softmax(tensor):
 
-    return np.exp(-tensor) / np.sum(np.exp(-tensor), axis=0)
+    return np.exp(tensor) / np.sum(np.exp(tensor), axis=0)
 
 
 def relu(tensor):